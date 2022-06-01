d4e8c3e6d54496fc56d695b0a66ab66d943f719d
luchaotb <anuta.luchi8@gmail.com>
2022-06-01 05:19:48 +0300

    Basics

--- main.py
+++ main.py
@@ -6,10 +6,30 @@ main = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@main.route('/xvii')
+def xvii():
+    return render_template("xvii.html")
 
-@main.route('/about')
-def about():
-    return "i am home"
+@main.route('/xviii')
+def xviii():
+    return render_template("xviii.html")
+
+@main.route('/xix')
+def xix():
+    return render_template("xix.html")
+
+@main.route('/xx')
+def xx():
+    return render_template("xx.html")
+
+@main.route('/xxi')
+def xxi():
+    return render_template("xxi.html")
+
+
+@main.route('/mail')
+def mail():
+    return render_template("mail.html")
 
 
 if __name__ == "__main__":
